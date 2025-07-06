b6fc6cad6632062af8812f8a1a975834ff47e3a9
Vladimir Kipriyanov <liar2707@gmail.com>
2025-07-06 09:37:32 +0500

    final 1.4

--- src/Diff.php
+++ src/Diff.php
@@ -9,43 +9,42 @@ function diff(array $firstData, array $secondData): array
     $keys = array_unique(array_merge(array_keys($firstData), array_keys($secondData)));
     $sortedKeys = sortBy($keys, fn($key) => $key);
 
-    return array_reduce($sortedKeys, function ($diff, $key) use ($firstData, $secondData) {
-        $inFirst = array_key_exists($key, $firstData);
-        $inSecond = array_key_exists($key, $secondData);
-
-        if ($inFirst && $inSecond) {
-            $firstValue = $firstData[$key];
-            $secondValue = $secondData[$key];
-
-            if (is_array($firstValue) && is_array($secondValue)) {
-                $nestedDiff = diff($firstValue, $secondValue);
-                $diff[] = ['nested', $key, $nestedDiff];
-            } elseif ($firstValue !== $secondValue) {
-                $diff[] = ['updateDeleted', $key, is_array($firstValue)
-                    ? diff($firstValue, $firstValue)
-                    : $firstValue];
-                $diff[] = ['updateAdded', $key, is_array($secondValue)
-                    ? diff($secondValue, $secondValue)
-                    : $secondValue];
-            } else {
-                $diff[] = ['unchanged', $key, $firstValue];
+    return array_reduce(
+        $sortedKeys,
+        fn($diff, $key) => [
+            ...$diff,
+            ...match (
+                [
+                array_key_exists($key, $firstData),
+                array_key_exists($key, $secondData)
+                ]
+            ) {
+                [true, true] => match (true) {
+                    is_array($firstData[$key]) && is_array($secondData[$key]) => [
+                        ['nested', $key, diff($firstData[$key], $secondData[$key])]
+                    ],
+                    $firstData[$key] !== $secondData[$key] => [
+                        ['updateDeleted', $key, is_array($firstData[$key])
+                            ? diff($firstData[$key], $firstData[$key])
+                            : $firstData[$key]],
+                        ['updateAdded', $key, is_array($secondData[$key])
+                            ? diff($secondData[$key], $secondData[$key])
+                            : $secondData[$key]]
+                    ],
+                    default => [['unchanged', $key, $firstData[$key]]]
+                },
+                [true, false] => [
+                    is_array($firstData[$key])
+                        ? ['deletedNested', $key, diff($firstData[$key], $firstData[$key])]
+                        : ['deleted', $key, $firstData[$key]]
+                ],
+                [false, true] => [
+                    is_array($secondData[$key])
+                        ? ['addedNested', $key, diff($secondData[$key], $secondData[$key])]
+                        : ['added', $key, $secondData[$key]]
+                ]
             }
-        } elseif ($inFirst) {
-            $value = $firstData[$key];
-            if (is_array($value)) {
-                $diff[] = ['deletedNested', $key, diff($value, $value)];
-            } else {
-                $diff[] = ['deleted', $key, $value];
-            }
-        } elseif ($inSecond) {
-            $value = $secondData[$key];
-            if (is_array($value)) {
-                $diff[] = ['addedNested', $key, diff($value, $value)];
-            } else {
-                $diff[] = ['added', $key, $value];
-            }
-        }
-
-        return $diff;
-    }, []);
+        ],
+        []
+    );
 }
